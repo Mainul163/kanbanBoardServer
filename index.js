0721d3407ac4ea548997ade476b8ccc71eebe409
mainul163 <mainulisraf163@gmail.com>
2022-09-04 16:24:43 +0600

    inprogress api done

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const { MongoClient, ServerApiVersion } = require("mongodb");
 var cors = require("cors");
 const app = express();
 app.use(cors());
+let objectId=require('mongodb').ObjectID
 app.use(express.json());
+
 const port = process.env.PORT || 5000;
 
 const uri =
@@ -19,18 +21,67 @@ async function run() {
     await client.connect();
 
     const collection = client.db("kanbanBoard").collection("task");
+    
+    app.get('/kanbanboard',async (req,res)=>{
+
+      const cursor = collection.find();
+      const result= await cursor.toArray();
+      res.send(result)
+    })
+
     app.post("/kanbanboard", async (req, res) => {
       const task = req.body;
 
       const result = await collection.insertOne(task);
       res.send(result);
     });
+    app.delete("/kanbanboard/:id",async(req,res)=>{
+      let taskId=req.params.id
+     
+      const query = { id: parseInt(taskId)  };
+      
+      const result = await collection.deleteOne(query);
+     res.send(result)
+     })
+  } finally {
+    // await client.close();
+  }
+}
+
+async function inprogress() {
+  try {
+    await client.connect();
+
+    const collection = client.db("kanbanBoardInprogress").collection("inprogress");
+    
+    app.get('/kanbanboard/inprogress',async (req,res)=>{
+
+      const cursor = collection.find();
+      const result= await cursor.toArray();
+      res.send(result)
+    })
+
+    app.post("/kanbanboard/inprogress", async (req, res) => {
+      const task = req.body;
+
+      const result = await collection.insertOne(task);
+      res.send(result);
+    });
+
+    app.delete("/kanbanboard/inprogress/:id",async(req,res)=>{
+     let taskId=req.params.id
+     console.log(taskId);
+     const query = { id: parseInt(taskId)  };
+     console.log(query);
+     const result = await collection.deleteOne(query);
+    res.send(result)
+    })
   } finally {
     // await client.close();
   }
 }
 run().catch(console.dir);
-
+inprogress().catch(console.dir);
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
